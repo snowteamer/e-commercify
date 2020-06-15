6bdfd3c7d7b0cd70d70ecbb74eb5783920f21381
snowteamer <64228468+snowteamer@users.noreply.github.com>
2020-06-15 20:12:58 +0200

    Fix type error during URL validation

--- src/schemas/db/productSchema.js
+++ src/schemas/db/productSchema.js
@@ -4,12 +4,12 @@ const { Schema } = require('mongoose');
 const discountSchema = require('./discountSchema');
 
 const partsOfRegex = [
-  '(https?://(?:www.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]',
-  '+[a-zA-Z0-9].[^s]{2,}|www.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9].[^s]{2,}',
-  '|https?://(?:www.|(?!www))[a-zA-Z0-9]+.[^s]{2,}|www.[a-zA-Z0-9]+.[^s]{2,})'
+  String.raw`(https?://(?:www.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]`,
+  String.raw`[a-zA-Z0-9].[^s]{2,}|www.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9].[^s]{2,}`,
+  String.raw`|https?://(?:www.|(?!www))[a-zA-Z0-9]+.[^s]{2,}|www.[a-zA-Z0-9]+.[^s]{2,})`
 ];
 
-const urlValidationRegex = partsOfRegex.join('');
+const urlValidationRegex = new RegExp(partsOfRegex.join(''));
 
 const productSchema = new Schema({
   productId: Schema.Types.ObjectID,
